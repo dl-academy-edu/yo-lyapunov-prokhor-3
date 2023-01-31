8ec5e96433ce76fad58ebd2d08dcac83cfbc95fa
Phonk05 <lyfy05@yandex.ru>
2023-01-31 16:07:20 +0300

    3 домашка

---
+++ home tusks/functions.js
@@ -0,0 +1,32 @@
+// Проверка на возраст
+
+function isEighteen() {
+    const num = prompt('Сколько вам лет?');
+    if (+num >= 18) return true;
+    isEighteen();
+}
+
+
+
+// Чистые функции 
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a - b;
+}
+
+// 3 ЗАДАНИЕ
+
+
+
+// 4 Задание
